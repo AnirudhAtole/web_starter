8919859ad3dd4d0ac8d3c6dec3cd9bb020baf0aa
Anirudh Atole <anirudhatole@gmail.com>
2023-01-06 15:09:20 +0530

    task 317

--- dom.js
+++ dom.js
@@ -3,8 +3,8 @@
 
 // console.log(document.getElementById('header-title'));
 
-var header_title = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var header_title = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 
 // header_title.textContent = 'hello'; 
 
@@ -16,22 +16,55 @@ var header = document.getElementById('main-header');
 
 // var h2header = document.getElementsByTagName('h2');
 
-// h2header[0].style.color = ' #00ff00 ';
-// h2header[0].style.fontWeight = 'bold';
+// // h2header[0].style.color = ' #00ff00 ';
+// // h2header[0].style.fontWeight = 'bold';
 
-//get element byclass
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
+// //get element byclass
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
 
-li[2].style.backgroundColor = 'Green';
+// li[2].style.backgroundColor = 'Green';
 
-for(let i = 0 ; i < li.length ; i++)
+// for(let i = 0 ; i < li.length ; i++)
+// {
+//     li[i].style.fontWeight = 'bold';
+//     li[i].style.color = 'red';
+// }
+
+// var licl = document.getElementsByClassName('list-broup2-item');
+// licl[0].style.color = 'green';
+// licl[0].fontWeight = 'bold';
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type = "submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var last_item = document.querySelector('.list-group-item:last-child');
+last_item.style.color = 'blue';
+
+var second_item = document.querySelector('.list-group-item:nth-child(2)');
+second_item.style.backgroundColor = 'green';
+
+var third_item = document.querySelector('.list-group-item:nth-child(3)');
+third_item.style.visibility = 'hidden';
+
+var items = document.querySelectorAll('.list-group-item');
+console.log(items);
+
+items[1].style.color = 'green';
+
+var odd_items = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i = 0 ; i < odd_items.length ; i++)
 {
-    li[i].style.fontWeight = 'bold';
-    li[i].style.color = 'red';
+    odd_items[i].style.backgroundColor = 'green';
 }
-
-var licl = document.getElementsByClassName('list-broup2-item');
-licl[0].style.color = 'green';
-licl[0].fontWeight = 'bold';
